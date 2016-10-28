5c2ec831a39228775111a31eea3f59557fea44f9
Taylor Parsell <tayparsell@gmail.com>
2016-10-28 13:37:22 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -1,84 +1,60 @@
-$(document).ready(function() {
+	$(document).ready(function() {
 
-/* 1. set up functions that will read through the text and analyze it for [totalWordCount], 
-   [uniqueWordCount], [avgWordLength], [avgSentenceLength]. 
-   2. set up listeners that will get you the submitted text and run it through the functions 
-   above
-*/
+	// 1. set up event listener for form submission & remove hidden class
+	$('button').click(function(event) {
+		$('dl').removeClass('hidden');
+	});
+
+	$(function() {
+	  $('#js-form').submit(function(event) {
+	    var textInput = $('#js-user-input').val();
+	    textAnalyzer(textInput);
+	  	event.preventDefault();
+	  })
+	});
+
+	// 2. set up functions that will read through the text and analyze it for 
+	// [totalWordCount], [uniqueWordCount], [avgWordLength], [avgSentenceLength]. 
+	function textAnalyzer(textInput) {
+		var wordsArray = textInput.toLowerCase().match(/\b[^\s]+\b/g).sort();
+ 
+		// total word count - divide into functions
+		var totalWordCount = wordsArray.length; 
+		$('#js-word-count').text(totalWordCount);
 
 
+		// unique word count
+		var uniqueWordCount = {};
 
-function textAnalyzer(textInput) {
-	var newArray = textInput.split(" ");
-	console.log(newArray);
+		for (var i = 0; i < wordsArray.length; i++) {
+			var word = wordsArray[i];
+			uniqueWordCount[word] = uniqueWordCount[word] ? uniqueWordCount[word] + 1 : 1;
+		}
 
-// total word count
-	var totalWordCount = newArray.length; 
-	//console.log(totalWordCount);
-	$('#js-word-count').text(totalWordCount);
+		var numberOfUniqueWords = Object.keys(uniqueWordCount).length;
+		$('#js-unique-count').text(numberOfUniqueWords);
 
-// unique word count	
-	var uniqueWordCount = {};
-	var uniqueCountAry = [];
 
-	for (var i = 0; i < newArray.length; i++) {
-		var count = newArray[i];
-		uniqueWordCount[count] = uniqueWordCount[count] ? uniqueWordCount[count] + 1 : 1;
+		// avg word length
+		var sum = 0;
+
+		for(var j = 0; j < wordsArray.length; j++) {
+	    	var wordsFromArray = wordsArray[j];
+	    	sum += wordsFromArray.length;
+		}
+		var avgWordLength = sum / wordsArray.length;
+		$('#avg-word-length').text(avgWordLength);
+
+
+		// avg sentence length
+		var sentences = textInput.split(".");
+		var sentenceSum = 0;
+		for (var k = 0; k < sentences.length; k++) {
+			var sentence = sentences[k];
+			sentenceSum += sentence.split(" ").length;
+		}
+		var avgSentenceLength = sentenceSum / (sentences.length);
+		$('#avg-sentence-length').text(avgSentenceLength);
 	}
-	for (var key in uniqueWordCount) {
-	uniqueCountAry.push(uniqueWordCount[key]); 
-	}
 
-	$('#js-unique-count').text(uniqueCountAry.length);
-	console.log(uniqueWordCount);
-	console.log(uniqueCountAry);
-
-// avg word length
-	var sum = 0;
-	for(var j = 0; j < newArray.length; j++) {
-    	var strLength = newArray[j];
-    	sum += strLength.length;
-	}
-	var avgWordLength = sum / newArray.length;
-	$('#avg-word-length').text(avgWordLength);
-
-	//console.log(avgWordLength)
-
-// avg sentence length
-	
-	var sentence = textInput.split("\n");
-	var sentenceSum = 0;
-	for (var k = 0; k < sentence.length; k++) {
-		var sentenceLen = sentence[k];
-	//console.log(sentence); //sentences separated into strings
-	//console.log(sentenceLen); //avg denominator
-		sentenceSum += sentenceLen.length;
-	}
-
-	var avgSentenceLength = sentenceSum / sentence.length;
-	$('#avg-sentence-length').text(avgSentenceLength);
-
-
-}
-
-//2. set up event listener for form submission & remove hidden class
-
-
-$('button').click(function(event) {
-	//event.preventDefault();
-	$('dl').removeClass('hidden');
-});
-
-
-$(function() {
-  $('#js-form').submit(function(event) {
-    var textInput = $('#js-user-input').val();
-    textAnalyzer(textInput);
-  	event.preventDefault();
-
-  })
-
-});
-
-
-}) //docready 
+})  
